e73db021fd7f9b27bad5fa251a09d3d2c26db180
2bt <s8572327@gmail.com>
2019-06-04 14:57:05 +0200

    stuff

--- player.go
+++ player.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"image"
+
 	"github.com/hajimehoshi/ebiten"
 	"github.com/hajimehoshi/ebiten/ebitenutil"
 )
@@ -12,14 +14,14 @@ func init() {
 }
 
 type Player struct {
-	x   float64
-	y   float64
-	dir float64
+	x    float64
+	y    float64
+	vx   float64
+	dir  float64
+	tick int
 }
 
 func (p *Player) Init() {
-	p.x = 0
-	p.y = 0
 	p.dir = 1
 }
 
@@ -31,15 +33,28 @@ func (p *Player) Update(input Input) {
 		p.dir = float64(input.x)
 	}
 
-	p.x += float64(input.x) * speed
+	p.vx = float64(input.x) * speed
+	p.x += p.vx
 	p.y += float64(input.y) * speed
 
+	p.tick++
 }
 
 func (p *Player) Draw(screen *ebiten.Image) {
 	o := ebiten.DrawImageOptions{}
-	o.GeoM.Scale(p.dir, 1)
+	if p.dir == -1 {
+		o.GeoM.Scale(p.dir, 1)
+		o.GeoM.Translate(16, 0)
+	}
 	o.GeoM.Translate(p.x, p.y)
-	screen.DrawImage(playerSprite, &o)
+
+	f := 0
+	if p.vx != 0 {
+		f = p.tick/4%2 + 1
+	}
+
+	frame := playerSprite.SubImage(image.Rect(f*16, 0, f*16+16, 16)).(*ebiten.Image)
+
+	screen.DrawImage(frame, &o)
 
 }
