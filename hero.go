b0328889eff32a50722c3ab5b443946485ab727a
2bt <s8572327@gmail.com>
2019-08-09 16:21:25 +0200

    WIP ladders

--- hero.go
+++ hero.go
@@ -18,6 +18,7 @@ type Hero struct {
 	vx          float64
 	vy          float64
 	dir         Dir
+	climbing    bool
 	inAir       bool
 	jumpControl bool
 	oldJump     bool
@@ -38,65 +39,120 @@ func (h *Hero) Update(input Input) {
 		h.dir = Left
 	}
 
-	// x movement and collision
-	accel := 0.5
-	if h.inAir {
-		accel = 0.125
-	}
-	if input.x != 0 {
-		h.vx += float64(input.x) * accel
-		h.vx = Clamp(h.vx, -MaxSpeedX, MaxSpeedX)
-	} else if h.vx > 0 {
-		h.vx = math.Max(0, h.vx-accel)
-	} else if h.vx < 0 {
-		h.vx = math.Min(0, h.vx+accel)
-	}
-	h.x += h.vx
+	if h.climbing {
 
-	dist := game.world.CheckCollision(AxisX, &Box{
-		h.x - 7, h.y - 19, 14, 19,
-	})
-	if dist != 0 {
-		h.x += dist
-	}
+		m := math.Mod(h.x, TileSize)
+		h.x += Clamp(8-m, -0.5, 0.5)
 
-	// y movement and collision
-	h.vy += Gravity
-	h.y += Clamp(h.vy, -MaxSpeedY, MaxSpeedY)
+		h.y += float64(input.y)
 
-	dist = game.world.CheckCollision(AxisY, &Box{
-		h.x - 7, h.y - 19, 14, 19,
-	})
-	if dist != 0 {
-		h.y += dist
-		h.vy = 0
-		if dist < 0 {
-			h.inAir = false
-		}
-	} else {
-		h.inAir = true
-	}
-
-	if h.inAir {
-		// jump higher
-		if h.jumpControl {
-			if !input.jump && h.vy < -1 {
-				h.vy = -1
-				h.jumpControl = false
-			}
-			if !input.jump || h.vy > -1 {
-				h.jumpControl = false
+		dist := game.world.CheckCollision(AxisY, &Box{
+			h.x - 7, h.y - 19, 14, 19,
+		})
+		if dist != 0 {
+			h.y += dist
+			h.vy = 0
+			if dist < 0 {
+				h.inAir = false
+				h.climbing = false
 			}
 		}
 
-	} else {
-		// jump
-		if input.jump && !h.oldJump {
+		x := int(h.x / TileSize)
+		y := int((h.y - 0.5) / TileSize)
+		t := game.world.TileAt(x, y)
+		if t != 'L' {
+			if h.dir == Right {
+				h.x += 1
+			} else {
+				h.x -= 1
+			}
+			h.climbing = false
+		}
+
+		if input.jump {
 			h.inAir = true
-			h.jumpControl = true
-			h.vy = -9
+			h.climbing = false
 		}
+
+	} else {
+
+		// x movement and collision
+		accel := 0.5
+		if h.inAir {
+			accel = 0.25
+		}
+		if input.x != 0 {
+			h.vx += float64(input.x) * accel
+			h.vx = Clamp(h.vx, -MaxSpeedX, MaxSpeedX)
+		} else if h.vx > 0 {
+			h.vx = math.Max(0, h.vx-accel)
+		} else if h.vx < 0 {
+			h.vx = math.Min(0, h.vx+accel)
+		}
+		h.x += h.vx
+
+		dist := game.world.CheckCollision(AxisX, &Box{
+			h.x - 7, h.y - 19, 14, 19,
+		})
+		if dist != 0 {
+			h.x += dist
+		}
+
+		// y movement and collision
+		h.vy += Gravity
+		h.y += Clamp(h.vy, -MaxSpeedY, MaxSpeedY)
+
+		dist = game.world.CheckCollision(AxisY, &Box{
+			h.x - 7, h.y - 19, 14, 19,
+		})
+		if dist != 0 {
+			h.y += dist
+			h.vy = 0
+			if dist < 0 {
+				h.inAir = false
+			}
+		} else {
+			h.inAir = true
+		}
+
+		if h.inAir {
+			// jump higher
+			if h.jumpControl {
+				if !input.jump && h.vy < -1 {
+					h.vy = -1
+					h.jumpControl = false
+				}
+				if !input.jump || h.vy > -1 {
+					h.jumpControl = false
+				}
+			}
+
+		} else {
+			// jump
+			if input.jump && !h.oldJump {
+				h.inAir = true
+				h.jumpControl = true
+				h.vy = -9
+			}
+		}
+
+		// check for latter
+		if input.y == -1 {
+			x := int(h.x / TileSize)
+			y := int((h.y - 0.5) / TileSize)
+			t := game.world.TileAt(x, y)
+			m := math.Mod(h.x, TileSize)
+			if t == 'L' && m > 5 && m < 11 {
+				h.inAir = false
+				h.climbing = true
+				h.vx = 0
+				h.vy = 0
+			}
+		}
+
 	}
+
 	h.oldJump = input.jump
 
 	// fire a bullet
